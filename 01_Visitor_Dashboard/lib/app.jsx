41eee640115443618ac63bc96f5095215f712b1f
Abhisek Jana <adeveloperdiary@gmail.com>
2016-04-20 11:56:38 -0400

    Version 1.3

--- 01_Visitor_Dashboard/lib/app.jsx
+++ 01_Visitor_Dashboard/lib/app.jsx
@@ -40,10 +40,45 @@ var Visitors = React.createClass({
     }
 });
 
+var Page = React.createClass({
+    render:function(){
 
-ReactDOM.render(<Browser/>,document.getElementById("browser"));
-ReactDOM.render(<RetVisitors/>,document.getElementById("ret_visitors"));
-ReactDOM.render(<Visitors/>,document.getElementById("top-line-chart"));
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12" >
+                        <div className="top fontSize">
+                            <a className="page-header" href="http://www.adeveloperdiary.com/d3-js/how-to-integrate-react-and-d3-the-right-way/"><h4 className="page-h4">(React + D3) Click here to view the full tutorial on adeveloperdiary.com</h4></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-12" >
+                        <div className="top" id="top-line-chart">
+                            <Visitors/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-7">
+                        <div className="bottom-left" id="browser">
+                            <Browser/>
+                        </div>
+                    </div>
+                    <div className="col-xs-5">
+                        <div className="bottom-right" id="ret_visitors">
+                            <RetVisitors/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+
+ReactDOM.render(<Page/>,document.getElementById("body"));
+
 
 
 
